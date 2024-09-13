76aa6fb4b69e1e1ca9a181bf3ef5a9d0f582bf9c
hieuzeus <hieu01bdvn@gmail.com>
2024-09-13 12:24:51 +0700

    init spring boot project

---
+++ src/main/java/devzeus/com/laptop_shop/controllers/UserController.java
@@ -0,0 +1,34 @@
+package devzeus.com.laptop_shop.controllers;
+
+import devzeus.com.laptop_shop.dtos.requests.UserDTO;
+import devzeus.com.laptop_shop.models.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/dev/v1")
+public class UserController {
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUser(@PathVariable("id") Long userId){
+        try {
+            User user = User.builder()
+                    .fullName("Hieu")
+                    .build();
+            return ResponseEntity.ok(user);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Cannot get user with id = " + userId +"\n" +e);
+        }
+    }
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody UserDTO userDTO, BindingResult bindingResult){
+        try {
+            User user = User.builder()
+                    .fullName("Hieu")
+                    .build();
+            return ResponseEntity.ok(user);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Cannot get user with id = " +"\n" +e);
+        }
+    }
+}
---
+++ src/main/java/devzeus/com/laptop_shop/dtos/requests/UserDTO.java
@@ -0,0 +1,11 @@
+package devzeus.com.laptop_shop.dtos.requests;
+
+import java.time.LocalDate;
+
+public class UserDTO {
+    private String phoneNumber;
+    private String fullName;
+    private String address;
+    private String password;
+    private LocalDate dayOfBirth;
+}
