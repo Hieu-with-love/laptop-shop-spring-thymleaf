0233ad9e03bc3c0266ebf5a7ba0b8bf6a7a90859
hieuzeus <hieu01bdvn@gmail.com>
2024-09-13 13:01:12 +0700

    add assets admin

--- src/main/java/devzeus/com/laptop_shop/controllers/UserController.java
+++ src/main/java/devzeus/com/laptop_shop/controllers/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/dev/v1")
+@RequestMapping("${api.prefix}/users")
 public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getUser(@PathVariable("id") Long userId){
@@ -30,5 +30,20 @@ public class UserController {
         }catch (Exception e){
             return ResponseEntity.badRequest().body("Cannot get user with id = " +"\n" +e);
         }
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable("id") Long userId,@RequestBody UserDTO userDTO, BindingResult bindingResult){
+        try {
+            User user = User.builder()
+                    .fullName("Hieu")
+                    .build();
+            return ResponseEntity.ok(user);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Cannot get user with id = " +"\n" +e);
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") Long userId){
+        return ResponseEntity.ok("Delete successful");
     }
 }
