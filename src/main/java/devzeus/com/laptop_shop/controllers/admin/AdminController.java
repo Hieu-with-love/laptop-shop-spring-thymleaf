38c3631d737beca72192bdca0ecca536d80d04f5
hieuzeus <hieu01bdvn@gmail.com>
2024-09-15 08:35:55 +0700

    add adminpage

---
+++ src/main/java/devzeus/com/laptop_shop/controllers/admin/AdminController.java
@@ -0,0 +1,12 @@
+package devzeus.com.laptop_shop.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin/index";
+    }
+}
