1761ea29b7b19b8ac1652eff6f889cea1ee9f57b
hieuzeus <hieu01bdvn@gmail.com>
2024-09-16 05:01:30 +0700

    fragment for admin home

--- src/main/java/devzeus/com/laptop_shop/controllers/admin/AdminController.java
+++ src/main/java/devzeus/com/laptop_shop/controllers/admin/AdminController.java
@@ -1,12 +1,22 @@
 package devzeus.com.laptop_shop.controllers.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
-    @RequestMapping("/admin")
-    public String admin() {
-        return "admin/html/index";
+    @GetMapping()
+    public String admin(Model model) {
+        model.addAttribute("pageTitle", "Home Admin");
+        return "index";
+    }
+
+    @GetMapping("/categories")
+    public String categories(Model model) {
+        model.addAttribute("pageTitle", "Categories");
+        return "admin/html/categories";
     }
 }
