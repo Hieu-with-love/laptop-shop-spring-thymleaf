4529180b706831d6d32752dfb4869472efd4ffe3
hieuzeus <hieu01bdvn@gmail.com>
2024-09-16 22:57:09 +0700

    add user and fragments_user folder for user page

---
+++ src/main/java/devzeus/com/laptop_shop/controllers/admin/AdminProductController.java
@@ -0,0 +1,25 @@
+package devzeus.com.laptop_shop.controllers.admin;
+
+import devzeus.com.laptop_shop.models.Product;
+import devzeus.com.laptop_shop.services.classes.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/products")
+@RequiredArgsConstructor
+public class AdminProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public String showProducts(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "admin/cruds/products";
+    }
+}
