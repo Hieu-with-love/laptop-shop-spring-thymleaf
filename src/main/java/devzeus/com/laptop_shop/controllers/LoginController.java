2c9fb9e58b98a9cc0bdbe01324f07b0fa8c272fd
hieuzeus <hieu01bdvn@gmail.com>
2024-10-07 15:34:20 +0700

    modifier | changes login to new location, and fix some bugs

--- src/main/java/devzeus/com/laptop_shop/controllers/LoginController.java
+++ src/main/java/devzeus/com/laptop_shop/controllers/LoginController.java
@@ -1,7 +1,6 @@
-package devzeus.com.laptop_shop.controllers.admin;
+package devzeus.com.laptop_shop.controllers;
 
 import devzeus.com.laptop_shop.dtos.requests.UserDTO;
-import devzeus.com.laptop_shop.models.User;
 import devzeus.com.laptop_shop.services.classes.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-public class LoginControllerAdmin {
+public class LoginController {
     private final UserDetailsService userDetailsService;
     private final UserService userService;
 
@@ -34,13 +32,13 @@ public class LoginControllerAdmin {
     @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("myUser", new UserDTO());
-        return "admin/login/login";
+        return "login/login";
     }
 
     @GetMapping("/register")
     public String showRegister(Model model) {
         model.addAttribute("newUserDTO", new UserDTO());
-        return "admin/login/register";
+        return "login/register";
     }
 
     @PostMapping("/register")
@@ -49,12 +47,12 @@ public class LoginControllerAdmin {
         // Kiểm tra nếu số điện thoại đã tồn tại
         if (userService.getUserByPhoneNumber(userDTO.getPhoneNumber()) != null) {
             model.addAttribute("error", "Số điện thoại đã tồn tại");
-            return "/admin/login/register";
+            return "login/register";
         }
         // Đăng ký người dùng mới
         if (!userDTO.getPassword().equals(userDTO.getConfirmPassword())) {
             model.addAttribute("error", "Mật khẩu xác nhận không khớp");
-            return "/admin/login/register";
+            return "login/register";
         }
         userService.registerUser(userDTO);
         return "redirect:/login"; // Chuyển hướng đến trang đăng nhập sau khi đăng ký thành công
