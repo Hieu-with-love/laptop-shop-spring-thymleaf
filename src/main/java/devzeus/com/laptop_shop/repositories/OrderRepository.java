7eed874093e551f126a9bab8c245a6c23293fbcd
hieuzeus <hieu01bdvn@gmail.com>
2024-11-07 16:41:50 +0700

    add new assets for admin

--- src/main/java/devzeus/com/laptop_shop/repositories/OrderRepository.java
+++ src/main/java/devzeus/com/laptop_shop/repositories/OrderRepository.java
@@ -8,11 +8,6 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
-@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByPhoneNumber(String phoneNumber);
-    List<Order> findByAddressContaining(String address);
-    List<Order> findByTotalMoneyBetween(BigDecimal min, BigDecimal max);
-    List<Order> findByOrderDate(LocalDate orderDate);
-    List<Order> findAllByActive(boolean isActive);
+    
 }
